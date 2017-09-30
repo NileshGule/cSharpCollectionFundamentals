521ec06d5150016f5dea7d96d8d6a767a57f5d69
NileshGule <vn_nilesh@yahoo.com>
2017-09-30 22:54:50 +0800

    multiple modes of array initialization

--- Arrays/ArrayInitializer.cs
+++ Arrays/ArrayInitializer.cs
@@ -9,6 +9,15 @@ namespace Arrays
             int[] x1 = new int[5];
 
             Console.WriteLine($"Size of array = {x1.Length}");
+
+            // Explicit initialization with size specified
+            int[] x5 = new int[5] {1, 2, 3, 4, 5};
+
+            // Compiler is intelligent to compute the size
+            int[] x6 = {2, 3, 4, 5, 6, 7, 8}
+
+            // Compiler once again computes the size of array
+            int[] x7 = new int[] {1,2 , 3, 4, 6};
         }
     }
 }
