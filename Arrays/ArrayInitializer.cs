a6e7669ec6a20327ebc41f77855011c8305af6af
NileshGule <vn_nilesh@yahoo.com>
2017-09-30 22:48:40 +0800

    Added initializer class, updated StarupObject in csproj file

---
+++ Arrays/ArrayInitializer.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Arrays
+{
+    class ArrayInitializer
+    {
+        static void Main(string[] args)
+        {
+            int[] x1 = new int[5];
+
+            Console.WriteLine($"Size of array = {x1.Length}");
+        }
+    }
+}
