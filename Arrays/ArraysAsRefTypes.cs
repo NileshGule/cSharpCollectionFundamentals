07eaa97fd4fd3af7593d75fe408066cc4d8a5aeb
NileshGule <vn_nilesh@yahoo.com>
2017-10-03 21:58:46 +0800

    Adding example for arrays as reference types

---
+++ Arrays/ArraysAsRefTypes.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Arrays
+{
+    class ArraysAsRefTypes
+    {
+        static void Main (string[] args)
+        {
+            int[] x1 = { 1 , 4, 9, 16 };
+
+            var x2 = x1;
+
+            x1[0] = 2;
+
+            Console.WriteLine($"ReferenceEquals(x1, x2): {ReferenceEquals(x1, x2)}"); 
+
+            int[] x3 = {1, 4, 9, 16};
+
+            Console.WriteLine($"ReferenceEquals(x1, x3): {ReferenceEquals(x1, x3)}"); 
+
+        }
+    }
+}
