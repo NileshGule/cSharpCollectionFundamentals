f4c1f8062041e05906da052921d54f4d4ee4b279
NileshGule <vn_nilesh@yahoo.com>
2017-10-22 13:08:30 +0800

    Adding Array reversal functionality

---
+++ Arrays/ReverseArray.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Linq;
+
+namespace Arrays
+{
+    class ReverseArray
+    {      
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            string[] originalDaysOfWeek = daysOfWeek.ToArray();
+
+            Console.WriteLine("Reverse using static Array.Reverse method");
+
+            Array.Reverse(daysOfWeek);
+
+            foreach (string day in daysOfWeek)
+            {
+                Console.WriteLine(day);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Reverse using static Array.Reverse method with start & stop index");
+
+            Array.Reverse(daysOfWeek, 1, 4);
+
+            foreach (string day in daysOfWeek)
+            {
+                Console.WriteLine(day);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Reverse using extension method from LINQ");
+
+            var linqReversed = originalDaysOfWeek.Reverse();
+
+            foreach (string day in linqReversed)
+            {
+                Console.WriteLine(day);
+            }
+        }   
+    }
+}
