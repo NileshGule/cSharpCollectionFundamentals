82c52308bf65d8c8a5dfe29cdd69ddf953d93052
NileshGule <vn_nilesh@yahoo.com>
2017-10-23 23:09:14 +0800

    Added class for finding elements

---
+++ Arrays/FindElements.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+
+namespace Arrays
+{
+    class FindElements
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = ArrayUtils.DaysOfWeek;
+                        
+            int indexOfTuesday = Array.IndexOf(daysOfWeek, "Tuesday");
+
+            Console.WriteLine($"Index of Tuesday = {indexOfTuesday} using Array.IndexOf method. {Environment.NewLine}");
+
+            int dayStartingWithW = Array.FindIndex(daysOfWeek, x => x.StartsWith('W'));
+
+            Console.WriteLine($"Day of the week starting with W = {daysOfWeek[dayStartingWithW]}");
+
+            string[] daysStartingWithS = Array.FindAll(daysOfWeek, x=>x.StartsWith('S')).ToArray();
+
+        }
+    }
+}
