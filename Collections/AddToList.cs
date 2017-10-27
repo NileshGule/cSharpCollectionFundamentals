260548b413191ee3ae58b0c6188b8fdd0dfbc261
NileshGule <vn_nilesh@yahoo.com>
2017-10-27 22:49:45 +0800

    Added new project & AddToList example

---
+++ Collections/AddToList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Collections
+{
+    static class AddToList
+    {
+        static void Main(string[] args)
+        {
+            List<string> primeMinisters = new List<string> {
+                "Narendra Modi",
+                "Manmohan Singh",
+                "Atal Bihari Vajpeyi",
+                "Rajiv Gandhi",
+                "Inder Kumar Gujaral",
+                "H D Devegowda"
+            };
+
+            Console.WriteLine("Indian Prime Ministers:");
+
+            foreach (string primeMinister in primeMinisters)
+            {
+                Console.WriteLine(primeMinister);
+            }
+        }
+    }
+}
