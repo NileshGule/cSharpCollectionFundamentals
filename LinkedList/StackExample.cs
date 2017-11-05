a510ca15d75d28f6653a3c807013758f4627ec97
Nilesh Gule <vn_nilesh@yahoo.com>
2017-11-05 22:33:38 +0800

    Updated stack example

--- LinkedList/StackExample.cs
+++ LinkedList/StackExample.cs
@@ -14,10 +14,30 @@ namespace LinkedList
             books.Push("Hit Refresh");
             books.Push("Continuous Delivery");
 
-            foreach(string book in books)
+            PrintBooks(books);
+
+            Console.WriteLine("Removing topmost book");
+            books.Pop();
+
+            PrintBooks(books);
+
+            Console.WriteLine("Peeking at the without removing from stack");
+            string topBook = books.Peek();
+
+            Console.WriteLine($"Topmost book = {topBook}");
+            PrintBooks(books);
+        }
+
+        private static void PrintBooks(Stack<string> books)
+        {
+            Console.WriteLine();
+
+            foreach (string book in books)
             {
                 Console.WriteLine(book);
             }
+
+            Console.WriteLine();
         }
     }
 }
