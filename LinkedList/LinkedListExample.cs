39a3a0cc23cf18abebcdf7ec166d168001f1f4d7
NileshGule <vn_nilesh@yahoo.com>
2017-11-05 12:47:20 +0800

    adding example for linkedlist

---
+++ LinkedList/LinkedListExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    static class LinkedListExample
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> indianPrimeMinisters = new LinkedList<string>();
+
+            indianPrimeMinisters.AddLast("Jawaharlal Nehru");
+            indianPrimeMinisters.AddLast("Lalbahadur Shastri");
+            indianPrimeMinisters.AddLast("Indira Gandhi");
+            indianPrimeMinisters.AddLast("Rajiv Gandhi");
+
+            foreach (string primeminister in indianPrimeMinisters)
+            {
+                Console.WriteLine(primeminister);
+            }
+
+            Console.WriteLine("Adding prime minister after Indira Gandhi");
+
+            
+        }
+    }
+}
