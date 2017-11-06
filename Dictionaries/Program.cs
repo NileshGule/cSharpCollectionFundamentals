3d8597073672ecc7dfef70840aad347ba536ce1a
Nilesh Gule <vn_nilesh@yahoo.com>
2017-11-06 22:35:35 +0800

    Added example of Add method

--- Dictionaries/Program.cs
+++ Dictionaries/Program.cs
@@ -36,6 +36,7 @@ namespace Dictionaries
 
             primeMinisters["AV"] = new PrimeMinister("Atal Bihari Vajpeyi", 1996);
 
+            primeMinisters.Add("NM", new PrimeMinister("Narendra Modi", 2014));
             foreach (var primeMinister in primeMinisters)
             {
                 Console.WriteLine($"Key : {primeMinister.Key}, Value : { primeMinister.Value }");
