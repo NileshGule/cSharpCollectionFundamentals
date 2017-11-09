342df7b10c9f48ddeaa5a1ced7d1ddb3044008f0
NileshGule <vn_nilesh@yahoo.com>
2017-11-09 22:50:46 +0800

    renamed class to DictionaryExample

--- Dictionaries/DictionaryExample.cs
+++ Dictionaries/DictionaryExample.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Dictionaries
 {
-    static class Program
+    static class DictionaryExample
     {
         static void Main(string[] args)
         {
