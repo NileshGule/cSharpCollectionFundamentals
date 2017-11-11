2cb339deab971acce0ea157eb64caaf52f628245
NileshGule <vn_nilesh@yahoo.com>
2017-11-11 20:21:38 +0800

    Added intersect & intersect with examples

---
+++ Sets/HashSetIntersectExample.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sets
+{
+    static class HashSetIntersectExample
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> bigCities = new HashSet<string>
+            {
+                "New York",
+                "Manchester",
+                "Sheffield",
+                "Paris"
+            };
+
+            string[] citiesInUK = {
+                "Manchester",
+                "Sheffield",
+                "Ripon",
+                "Truro"
+                };
+
+            Console.WriteLine("Using LINQ intersect method");
+            var commonCities = bigCities.Intersect(citiesInUK);
+            commonCities.ToList().ForEach(x => Console.WriteLine(x));
+
+            Console.WriteLine("Using Intersect method of HashSet class");
+            bigCities.IntersectWith(citiesInUK);
+
+            bigCities.ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
